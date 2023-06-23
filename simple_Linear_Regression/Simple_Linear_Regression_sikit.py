04f9cce5450bdf31c9e7c8ee78c9239a742f8c89
amin <aminzafary3@gmail.com>
2023-06-23 19:36:47 +0330

    final edition for linear regression by sikit learn

--- simple_Linear_Regression/Simple_Linear_Regression_sikit.py
+++ simple_Linear_Regression/Simple_Linear_Regression_sikit.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import SGDRegressor , LinearRegression
 from sklearn.preprocessing import StandardScaler
 np.set_printoptions(precision=2)
 #plt.style.use('./deeplearning.mplstyle')
@@ -41,3 +41,36 @@ print(f"prediction using np.dot() and sgdr.predict match: {(y_pred == y_pred_sgd
 
 print(f"Prediction on training set:\n{y_pred[:4]}" )
 print(f"Target values \n{y_train[:4]}")
+
+
+print("////////////////////////////////////////////////////////////////////////////////////////")
+#example for normal linear regression in sikit learn
+
+data = np.loadtxt("data/houses.txt", delimiter=',', skiprows=1)
+
+X_train = data[:,:4]
+y_train = data[:,4]
+X_features = ['size(sqft)','bedrooms','floors','age']
+
+scaler = StandardScaler()
+X_train_norm =  scaler.fit_transform(X_train)
+
+
+linear_model = LinearRegression()
+linear_model.fit(X_train, y_train) 
+
+b = linear_model.intercept_
+w = linear_model.coef_
+print(f"w = {w:}, b = {b:0.2f}")
+
+print(f"Prediction on training set:\n {linear_model.predict(X_train)[:4]}" )
+print(f"prediction using w,b:\n {(X_train @ w + b)[:4]}")
+print(f"Target values \n {y_train[:4]}")
+
+print(f"Prediction on training set:\n {linear_model.predict(X_train_norm)[:4]}" )
+print(f"prediction using w,b:\n {(X_train_norm @ w + b)[:4]}")
+print(f"Target values \n {y_train[:4]}")
+
+x_house = np.array([1200, 3,1, 40]).reshape(-1,4)
+x_house_predict = linear_model.predict(x_house)[0]
+print(f" predicted price of a house with 1200 sqft, 3 bedrooms, 1 floor, 40 years old = ${x_house_predict*1000:0.2f}")
