8514d56b9fb492c4cbc34384736b7d076f2a449c
amin <aminzafary3@gmail.com>
2023-07-18 16:28:17 +0330

    simple neural network for binary digit recognation between 0 and 1 with tensorflow and numpy implementation

---
+++ Simple_Neural_Networks/Simple_Neural_Network_Handwrite_Digit_Recognition_Binary_Numpy_implementation.py
@@ -0,0 +1,110 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+from Simple_Neural_Network_Handwrite_Digit_Recognition_Binary_Tensorflow import *
+#%matplotlib inline
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+def sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+
+X = np.load("data/X.npy")
+y = np.load("data/y.npy")
+X = X[0:1000]
+y = y[0:1000]
+
+#---------------------------------------------------implementation----------------------------------------------
+
+def my_dense(a_in, W, b, g):
+    """
+    Computes dense layer
+    Args:
+      a_in (ndarray (n, )) : Data, 1 example 
+      W    (ndarray (n,j)) : Weight matrix, n features per unit, j units
+      b    (ndarray (j, )) : bias vector, j units  
+      g    activation function (e.g. sigmoid, relu..)
+    Returns
+      a_out (ndarray (j,))  : j units
+    """
+    units = W.shape[1]
+    a_out = np.zeros(units)
+
+    for j in range(units):
+        w = W[:,j]
+        z = np.dot(w,a_in) + b[j]
+        a_out[j] = 1. / (1. + np.exp(-z))
+
+    return(a_out)
+
+#it builds a three-layer neural network
+def my_sequential(x, W1, b1, W2, b2, W3, b3):
+    a1 = my_dense(x,  W1, b1, sigmoid)
+    a2 = my_dense(a1, W2, b2, sigmoid)
+    a3 = my_dense(a2, W3, b3, sigmoid)
+    return(a3)
+
+
+
+W1_tmp,b1_tmp = layer1.get_weights()
+W2_tmp,b2_tmp = layer2.get_weights()
+W3_tmp,b3_tmp = layer3.get_weights()
+
+
+#----------------------------------------------making prediction-----------------------------------------------
+# make predictions
+prediction = my_sequential(X[0], W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print( "yhat = ", yhat, " label= ", y[0,0])
+prediction = my_sequential(X[500], W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print( "yhat = ", yhat, " label= ", y[500,0])
+
+
+#------------------------------------------visualizing prediction result---------------------------------------
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1,rect=[0, 0.03, 1, 0.92]) #[left, bottom, right, top]
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+
+    # Predict using the Neural Network implemented in Numpy
+    my_prediction = my_sequential(X[random_index], W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+    my_yhat = int(my_prediction >= 0.5)
+
+    # Predict using the Neural Network implemented in Tensorflow
+    tf_prediction = model.predict(X[random_index].reshape(1,400))
+    tf_yhat = int(tf_prediction >= 0.5)
+    
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]},{tf_yhat},{my_yhat}")
+    ax.set_axis_off() 
+fig.suptitle("Label, yhat Tensorflow, yhat Numpy", fontsize=16)
+plt.show()
+
---
+++ Simple_Neural_Networks/Simple_Neural_Network_Handwrite_Digit_Recognition_Binary_Tensorflow.py
@@ -0,0 +1,141 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+#from autils import *
+#%matplotlib inline
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+#----------------------------------------------------------------------------------------------------------------------------------
+#In this exercise, you will use a neural network to recognize two handwritten digits, zero and one. This is a binary classification task.
+#----------------------------------------------------------------------------------------------------------------------------------
+
+#loading data
+X = np.load("data/X.npy")
+y = np.load("data/y.npy")
+X = X[0:1000]
+y = y[0:1000]
+
+#check deminition of variables
+print ('The shape of X is: ' + str(X.shape))
+print ('The shape of y is: ' + str(y.shape))
+
+#----------------------------------------visualizing datas-------------------------------------------------
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1)
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Display the label above the image
+    ax.set_title(y[random_index,0])
+    ax.set_axis_off()
+plt.show()
+
+#----------------------------------------Tensorflow Model Implementation---------------------------------------
+model = Sequential(
+    [               
+        tf.keras.Input(shape=(400,)),    #specify input size
+
+        Dense(units=25,activation='sigmoid'),
+        Dense(units=15,activation='sigmoid'),
+        Dense(units=1,activation='sigmoid')
+    ], name = "my_model" 
+)                            
+
+
+
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    optimizer=tf.keras.optimizers.Adam(0.001),
+)
+
+model.fit(
+    X,y,
+    epochs=20
+)
+
+#--------------------------------------------Examine Weights shapes--------------------------------------
+#to see model units and other informations
+model.summary()
+
+
+#### Examine Weights shapes
+[layer1, layer2, layer3] = model.layers
+
+W1,b1 = layer1.get_weights()
+W2,b2 = layer2.get_weights()
+W3,b3 = layer3.get_weights()
+print(f"W1 shape = {W1.shape}, b1 shape = {b1.shape}")
+print(f"W2 shape = {W2.shape}, b2 shape = {b2.shape}")
+print(f"W3 shape = {W3.shape}, b3 shape = {b3.shape}")
+
+#----------------------------------------------making prediction---------------------------------------
+prediction = model.predict(X[0].reshape(1,400))  # a zero
+print(f" predicting a zero: {prediction}")
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print(f"prediction after threshold: {yhat}")
+
+prediction = model.predict(X[500].reshape(1,400))  # a one
+print(f" predicting a one:  {prediction}")
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print(f"prediction after threshold: {yhat}")
+
+#----------------------------------------------------visualizing after prediction---------------------------------------------
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1,rect=[0, 0.03, 1, 0.92]) #[left, bottom, right, top]
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Predict using the Neural Network
+    prediction = model.predict(X[random_index].reshape(1,400))
+    if prediction >= 0.5:
+        yhat = 1
+    else:
+        yhat = 0
+    
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]},{yhat}")
+    ax.set_axis_off()
+fig.suptitle("Label, yhat", fontsize=16)
+plt.show()
+
